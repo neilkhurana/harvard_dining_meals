0a0cfe7176aca21bfb79736c015e9fdf07a77aae
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-04-23 22:28:04 -0400

    All the theme is complete. I have added one tab which includes a calender feature.

--- harvard_dining_meals/app.R
+++ harvard_dining_meals/app.R
@@ -1,82 +1,161 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# #
+# # This is a Shiny web application. You can run the application by clicking
+# # the 'Run App' button above.
+# #
+# # Find out more about building applications with Shiny here:
+# #
+# #    http://shiny.rstudio.com/
+# #
+# library(shinythemes)
+# library(shiny)
+# library(gsheet)
+# library(tidyverse)
+# library(ggplot2)
+
 
 library(shiny)
-library(gsheet)
+library(shinydashboard)
 library(tidyverse)
 library(ggplot2)
-library(shinythemes)
+library(gsheet)
+library(DT)
+library(shinydashboard)
 
-
-huds <-gsheet2tbl('https://docs.google.com/spreadsheets/d/1S3vFDul1PeB84Zd8G5ewe1ocZ6ezidQzAp5gn1eiHtQ/edit#gid=0') %>% 
-  mutate(Date = as.Date(Date, "%m-%d-%Y")) %>% 
-  filter(Date >= "2019-01-25")
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Harvard University Dining Services Search Tool"),
-   
-   # Sidebar with a slider input for number of bins 
-  tabsetPanel(
-  tabPanel("Calendar", fluid = TRUE,
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30),
-         dateInput(inputId, label, value = NULL, min = NULL, max = NULL,
-                   format = "yyyy-mm-dd", startview = "month", weekstart = 0,
-                   language = "en", width = NULL)
-      ),
-      
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui <- dashboardPage(skin = "red",
+  dashboardHeader(title = "HUDS Dining"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("dashboard")),
+      menuItem("Calendar", tabName = "calendar", icon = icon("th"))
+    )
   ),
-  tabPanel("Choose Meals", fluid = TRUE,
-           sidebarLayout(
-             sidebarPanel(
-               sliderInput("bins",
-                           "Number of bins:",
-                           min = 1,
-                           max = 50,
-                           value = 30)
-             ),
-             
-             # Show a plot of the generated distribution
-             mainPanel(
-               plotOutput("distPlot")
-             )
-           )
-  )
-)
-)
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    tabItems(
+      # First tab content
+      tabItem(tabName = "home",
+              fluidRow(
+                box(
+                  title = "Controls",
+                  selectInput("meal", "Meal:",
+                              c("Breakfast",
+                                "Lunch",
+                                "Dinner")),
+                  dateInput("date", label = "Date Input", value = NULL, min = NULL, max = NULL,
+                            format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                            language = "en", width = NULL)
+                ),
+                
+                DTOutput("table")
+                
+              )),
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+      tabItem(tabName = "calendar",
+              h2("Widgets tab content")
+        )
+      )
+    )
+      
+      # Second tab content
+      
+    )
+
+
+
+
+server <- function(input, output) {
+  
+  huds <-gsheet2tbl('https://docs.google.com/spreadsheets/d/1S3vFDul1PeB84Zd8G5ewe1ocZ6ezidQzAp5gn1eiHtQ/edit#gid=0') %>%
+    mutate(Date = as.Date(Date, "%m-%d-%Y")) %>%
+    filter(Date >= "2019-01-25")
+  
+  output$table <- renderDT({
+    huds %>% 
+      filter(Date == input$date) %>%
+      filter(Meal == input$meal) %>% 
+      select(Food) 
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
 
+
+# 
+# shinyApp(ui = ui, server = server)
+# # 
+# 
+
+# 
+# # Define UI for application that draws a histogram
+# ui <- dashboardPage(
+#    
+#    # Application title
+#    dashboardHeader(title = "HUDS"),
+#    dashboardSidebar(
+#      # There are a total of four main tabs, followed by a snapshot for a commodity. 
+#      # I added interesting icons to match the topics of the tabs. 
+#      sidebarMenu(
+#        menuItem("Calendar", tabName = "dashboard", icon = icon("home")),
+#        menuItem("Choose Meals", tabName = "graphs", icon = icon("align-left"))
+#      )
+#    ),
+#    
+#    
+#    dashboardBody(
+#      
+#   tabItems(
+#    
+#    tabItem(tabName = "dashboard", selectInput("meal", "Meal:",
+#                                               c("Breakfast",
+#                                                 "Lunch",
+#                                                 "Dinner")),
+#            dateInput("date", label = "Date Input", value = NULL, min = NULL, max = NULL,
+#                      format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+#                      language = "en", width = NULL), tableOutput("table")),
+#           tabItem(tabName = "graphs", sliderInput("bins",
+#                                            "Number of bins:",
+#                                            min = 1,
+#                                            max = 50,
+#                                            value = 30))
+#   )
+# )
+# )
+#    
+#            
+#       
+#    
+# #    # Sidebar with a slider input for number of bins 
+# #   tabsetPanel(
+# #   tabPanel("Calendar", fluid = TRUE,
+# #    sidebarLayout(
+# #       sidebarPanel(
+# #         
+# #       ),
+# #       
+# #       
+# #       # Show a plot of the generated distribution
+# #       mainPanel(
+# #          
+# #       )
+# #    )
+# #   ),
+# #   tabPanel("Choose Meals", fluid = TRUE,
+# #            sidebarLayout(
+# #              sidebarPanel(
+# #                
+# #              ),
+# #              
+# #              # Show a plot of the generated distribution
+# #              mainPanel(
+# #                tableOutput("distPlot")
+# #              )
+# #            )
+# #   )
+# # )
+# # )
+# # Define server logic required to draw a histogram
+
+# 
+# # Run the application 
+# shinyApp(ui = ui, server = server)
+# 
